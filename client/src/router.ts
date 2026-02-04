8574cbbfcecffae5dc8ce35f9ce7273f11c53a24
Keval <kevaldotdev@gmail.com>
2026-02-04 00:22:47 +0000

    web hash history

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import AgGrid from './components/AgGrid.vue'
 import Textile from './pages/Textile.vue'
 
@@ -9,7 +9,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes
 })
 
